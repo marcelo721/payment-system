5fe6bdb20a69235628ae7ff0e4f2531bc0aa283f
Marceo Henrique de Sousa <marceloh.sousa@alu.ufc.br>
2024-10-23 20:19:19 -0300

    first commit

---
+++ src/test/java/com/marcelo721/payment_system/PaymentSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marcelo721.payment_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
