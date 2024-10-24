ed02b2d4a0d9ffe0c8ad84bda36183c62b2741c0
Marceo Henrique de Sousa <marceloh.sousa@alu.ufc.br>
2024-10-24 15:05:45 -0300

    implemented ErrorMessage class and ApiExceptionHandler Class in Web package

---
+++ src/main/java/com/marcelo721/payment_system/web/exceptions/ApiExceptionHandler.java
@@ -0,0 +1,7 @@
+package com.marcelo721.payment_system.web.exceptions;
+
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ApiExceptionHandler {
+}
