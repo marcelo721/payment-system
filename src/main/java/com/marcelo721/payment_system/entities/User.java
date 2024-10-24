77bad1ac5c20407db04e055d3ee601b2b5d1b761
Marceo Henrique de Sousa <marceloh.sousa@alu.ufc.br>
2024-10-24 08:41:15 -0300

    created UserService class, UserRepository class, UserController class and some packages

---
+++ src/main/java/com/marcelo721/payment_system/entities/User.java
@@ -0,0 +1,52 @@
+package com.marcelo721.payment_system.entities;
+
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@RequiredArgsConstructor
+public class User implements UserDetails {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+
+    @Column(unique = true,name = "email")
+    private String email;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "verification_code")
+    private String verificationCode;
+
+    @Column(name = "enable")
+    private Boolean enabled;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return this.enabled;
+    }
+}
---
+++ src/main/java/com/marcelo721/payment_system/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.marcelo721.payment_system.repositories;
+
+import com.marcelo721.payment_system.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
