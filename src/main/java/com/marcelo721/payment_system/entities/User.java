a0ac50d6411e3a51fba8806ad382bf527f2b2181
Marceo Henrique de Sousa <marceloh.sousa@alu.ufc.br>
2024-11-06 09:28:09 -0300

    refatored enum type to String

--- src/main/java/com/marcelo721/payment_system/entities/User.java
+++ src/main/java/com/marcelo721/payment_system/entities/User.java
@@ -36,6 +36,7 @@ public class User implements UserDetails {
     private String verificationCode;
 
     @Column(name = "enabled")
+    @Enumerated(EnumType.STRING)
     private StatusAccount enabled;
 
     @Override
